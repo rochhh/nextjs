e52e7001b7451a4d2bfb3234dd1d0ebcb3f4a5b8
Roch <rochishnu7@gmail.com>
2023-06-17 17:44:32 +0530

    updated links from localhost to domain

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,10 +11,10 @@ export default function RootLayout({ children }) {
       <div>
         <div className=' sticky top-0 flex flex-wrap items-center bg-check  px-4 py-2  mx-auto bg-slate-50 navbarBorder ' >
           <nav className=' flex justify-center' >
-            <Link href="http://localhost:3000" className='self-center font-semibold text-lg ml-20 mr-20 py-4 px-6 text-lime-900 hover:text-emerald-700 ml ' > Roch </Link>
-            <Link href="/exp" className=' font-semibold text-lg ml-20 mr-20 py-4 px-6 text-lime-900 hover:text-emerald-700 ml ' >Experience </Link>
+            <Link href="https://rochishnuresume.vercel.app/" className='self-center font-semibold text-lg ml-20 mr-20 py-4 px-6 text-lime-900 hover:text-emerald-700 ml ' > Roch </Link>
+            <Link href="https://rochishnuresume.vercel.app/exp" className=' font-semibold text-lg ml-20 mr-20 py-4 px-6 text-lime-900 hover:text-emerald-700 ml ' >Experience </Link>
             <Link href="https://drive.google.com/file/d/12GlOof_b2W16fv811N5nOS6f6_W4RuU3/view?usp=sharing" className=' font-semibold text-lg ml-20 mr-20 py-4 px-6 text-lime-900 hover:text-emerald-700 ml ' > Resume </Link>
-            <Link href="/aboutme" className=' font-semibold text-lg ml-20 mr-20 py-4 px-6 text-lime-900 hover:text-emerald-700 ml ' > About me </Link>
+            <Link href="https://rochishnuresume.vercel.app/aboutme" className=' font-semibold text-lg ml-20 mr-20 py-4 px-6 text-lime-900 hover:text-emerald-700 ml ' > About me </Link>
           </nav>
           <Image src="/aws_thumb_image.png" width={400} height={300} className='changePos ' /> 
         </div>
